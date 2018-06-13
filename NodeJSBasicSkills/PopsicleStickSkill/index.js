c2b8f00e42595698f8687967c8e12fc61b0b856e
codetechlg <codetechlg@gmail.com>
2018-06-13 13:32:09 -0700

    created javascript file for popsicle stick skill
    subject to change

---
+++ NodeJSBasicSkills/PopsicleStickSkill/index.js
@@ -0,0 +1,63 @@
+'use strict';
+const Alexa = require('alexa-sdk');
+
+//Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.
+//Make sure to enclose your value in quotes, like this: const APP_ID = 'amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1';
+const APP_ID = undefined;
+
+//Alexa will speak out the help message when a user says help
+//For an idea of what you should say
+//-> "You can say tell me a space fact, or, you can say exit... What can I help you with?"
+const HELP_MESSAGE = 'HELP MESSAGE';
+const HELP_REPROMPT = 'What can I help you with?';
+
+//Alexa will speak out the stop message when the program ends
+const STOP_MESSAGE = 'Goodbye!';
+const FALLBACK_MESSAGE = 'I dont recognize that';
+
+const handlers = {
+    'LaunchRequest': function () {
+      this.emit('PickIntent');
+    },
+    'PickIntent': function () {
+        var studentList = ["Johnny Appleseed", "Hector Medina", "Celine Tran", "Mitchell Wang", "Caroline Zhu"];
+        
+        var random = Math.floor(Math.random()*studentList.length);
+        
+        var speechOutput = "I choose " + studentList[random]; 
+        
+        var speechReprompt = "Did you want another student?";
+        this.response.speak(speechOutput).listen(speechReprompt);
+        this.emit(':responseReady');
+    },
+    'AMAZON.HelpIntent': function () {
+        const speechOutput = HELP_MESSAGE;
+        const reprompt = HELP_REPROMPT;
+
+        this.response.speak(speechOutput).listen(reprompt);
+        this.emit(':responseReady');
+    },
+    'AMAZON.CancelIntent': function () {
+        this.response.speak(STOP_MESSAGE);
+        this.emit(':responseReady');
+    },
+    'AMAZON.StopIntent': function () {
+        this.response.speak(STOP_MESSAGE);
+        this.emit(':responseReady');
+    },
+     'AMAZON.FallbackIntent': function () {
+        this.response.speak(FALLBACK_MESSAGE);
+        this.emit(':responseReady').listen(HELP_REPROMPT);
+    },
+};
+
+//=========================================================================================================================================
+//Editing anything below this line might break your skill.
+//=========================================================================================================================================
+
+exports.handler = function (event, context, callback) {
+    const alexa = Alexa.handler(event, context, callback);
+    alexa.APP_ID = APP_ID;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
